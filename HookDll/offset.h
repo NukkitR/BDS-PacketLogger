058a74bd846186bc316c85e9d684de9f8a17e2bf
MagicDroidX <magicdroidx@gmail.com>
2021-09-24 13:45:28 +0800

    Update offsets (1.17.30)

--- HookDll/offset.h
+++ HookDll/offset.h
@@ -4,7 +4,13 @@
 
 namespace offset {
 
-	const uint64_t fn_Packet_ReadExtended = 0x3C96D0;
+	/*
+	CompleteResult *__fastcall Packet::readExtended(
+		Packet* packet, 
+		ReadOnlyBinaryStream *stream, 
+		PVOID a3)
+	*/
+	const uint64_t fn_Packet_ReadExtended = 0x348C70;
 	/*
 	Connection *__fastcall NetworkHandler::_sendInternal(
         NetworkHandler* this,
@@ -12,5 +18,5 @@ namespace offset {
         Packet *packet,
         PVOID packetBody)
 	*/
-	const uint64_t fn_NetworkHandler_SendInternal = 0x6B4B60;
+	const uint64_t fn_NetworkHandler_SendInternal = 0x62B250;
 }
